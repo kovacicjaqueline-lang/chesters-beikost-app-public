086c8199b7c42b074fe28157faa37b92b6487ed0
kovacicjaqueline-lang <316451114+kovacicjaqueline-lang@users.noreply.github.com>
2026-09-02 16:03:16 +0200

    test: preserve existing static-server mime type

--- browser-tests/save-ui-latency-webkit.test.mjs
+++ browser-tests/save-ui-latency-webkit.test.mjs
@@ -11,7 +11,7 @@ const mimeTypes = {
   ".js": "text/javascript; charset=utf-8",
   ".css": "text/css; charset=utf-8",
   ".json": "application/json; charset=utf-8",
-  ".webmanifest": "application/manifest+json",
+  ".webmanifest": "application/manifest+json; charset=utf-8",
   ".svg": "image/svg+xml",
   ".png": "image/png",
   ".webp": "image/webp",

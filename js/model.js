63bf58722b935003abe7d1ebb06e2e341b8a9df5
kovacicjaqueline-lang <316451114+kovacicjaqueline-lang@users.noreply.github.com>
2026-09-21 18:46:50 +0200

    Avoid broad inventory cache invalidation in hot path

--- js/model.js
+++ js/model.js
@@ -404,7 +404,6 @@ function inventoryName(item) {
     : food(item.foodId)?.name || item.foodName || "Lebensmittel";
 }
 function recipeInventoryPortions(recipeName) {
-  if (typeof invalidateInventoryAggregateCache === "function") invalidateInventoryAggregateCache();
   if (typeof inventoryRecipePortions === "function") return inventoryRecipePortions(recipeName);
   return state.inventory
     .filter(
